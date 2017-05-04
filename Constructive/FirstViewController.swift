6535a55dacf508174c39cd6be659ee71ec33995a
Radu Dutzan <radutzan@gmail.com>
2017-05-04 14:24:24 -0300

    Initial Commit

---
+++ Constructive/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Constructive
+//
+//  Created by Radu Dutzan on 5/4/17.
+//  Copyright © 2017 Radu Dutzan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
